7183ecb3409a3b5c8b4553ffcebf91cb7ea5f942
MartkCz <martk494@gmail.com>
2022-01-17 01:25:42 +0100

    add control nette renderer

--- src/Html/StimulusAttributeRenderer.php
+++ src/Html/StimulusAttributeRenderer.php
@@ -2,10 +2,20 @@
 
 namespace WebChemistry\Stimulus\Html;
 
+use Nette\Forms\Controls\BaseControl;
 use WebChemistry\Stimulus\Builder\AttributesBuilder;
 
 final class StimulusAttributeRenderer
 {
+
+	public static function control(BaseControl $control, StimulusAttributeRenderable ...$fragments): BaseControl
+	{
+		foreach (self::toArray(...$fragments) as $name => $value) {
+			$control->setHtmlAttribute($name, $value);
+		}
+
+		return $control;
+	}
 
 	public static function render(StimulusAttributeRenderable ... $fragments): string
 	{
@@ -18,6 +28,9 @@ final class StimulusAttributeRenderer
 		return $builder->toString();
 	}
 
+	/**
+	 * @return array<string, string>
+	 */
 	public static function toArray(StimulusAttributeRenderable ... $fragments): array
 	{
 		$builder = new AttributesBuilder();
