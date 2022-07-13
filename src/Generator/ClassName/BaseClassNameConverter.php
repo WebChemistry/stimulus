b171af5ca6692bdc4c97b2cea5d24de548786f85
MartkCz <martk494@gmail.com>
2022-07-13 15:02:32 +0200

    add classNameDecorator

--- src/Generator/ClassName/BaseClassNameConverter.php
+++ src/Generator/ClassName/BaseClassNameConverter.php
@@ -7,13 +7,19 @@ use Nette\Utils\Strings;
 final class BaseClassNameConverter implements ClassNameConverter
 {
 
+	/** @var (callable(string): string)|null */
+	private $classNameDecorator;
+
 	/**
 	 * @param string[] $keywords
+	 * @param (callable(string): string)|null $classNameDecorator
 	 */
 	public function __construct(
 		private array $keywords = [],
+		callable $classNameDecorator = null,
 	)
 	{
+		$this->classNameDecorator =$classNameDecorator;
 	}
 
 	/**
@@ -26,13 +32,17 @@ final class BaseClassNameConverter implements ClassNameConverter
 		foreach (explode('--', $name) as $namespace) {
 			if (isset($this->keywords[$namespace])) {
 				$names[] = $this->keywords[$namespace];
-				
+
 				continue;
 			}
 
 			$names[] = ucfirst(Strings::replace($namespace, '#-([a-zA-Z])#', function (array $matches): string {
 				return ucfirst($matches[1]);
 			}));
+		}
+
+		if ($this->classNameDecorator && ($lastKey = array_key_last($names)) !== null) {
+			$names[$lastKey] = ($this->classNameDecorator)($names[$lastKey]);
 		}
 
 		return implode('\\', $names); // @phpstan-ignore-line
