5c93a191014ef8adaad88d92a6988574c7e2707e
MartkCz <martk494@gmail.com>
2023-09-12 17:02:14 +0200

    latte: yield sub-nodes

--- src/Latte/Extension/Node/StimulusNode.php
+++ src/Latte/Extension/Node/StimulusNode.php
@@ -2,6 +2,7 @@
 
 namespace WebChemistry\Stimulus\Latte\Extension\Node;
 
+use Generator;
 use Latte\Compiler\Nodes\Php\Expression\ArrayNode;
 use Latte\Compiler\Nodes\StatementNode;
 use Latte\Compiler\PrintContext;
@@ -11,7 +12,7 @@ use WebChemistry\Stimulus\Latte\StimulusMacroService;
 final class StimulusNode extends StatementNode
 {
 
-	private ArrayNode $arguments;
+	public ArrayNode $arguments;
 
 	public static function create(Tag $tag): self
 	{
@@ -29,6 +30,11 @@ final class StimulusNode extends StatementNode
 			sprintf('echo " " . %s::render(%%args);', StimulusMacroService::class),
 			$this->arguments,
 		);
+	}
+
+	public function &getIterator(): Generator
+	{
+		yield $this->arguments;
 	}
 
 }
